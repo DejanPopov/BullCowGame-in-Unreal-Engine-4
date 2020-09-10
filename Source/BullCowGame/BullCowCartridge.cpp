dd39d51663fdedb9eff544df307a7508d6128367
Dejan Popov <dekipopica92@gmail.com>
2020-09-10 14:30:44 +0200

    Added IF  - .Len()
    Added another If thech check the length of characters  of Input and comparing them to the HiddenWord

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -6,9 +6,9 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     Super::BeginPlay();
     PrintLine(TEXT("Hello there!"));
     PrintLine(TEXT("Guess the 4 letter word")); //Number is hardcoded REMOVE latter!
-    PrintLine(TEXT("Press ENter to continue..."));
+    PrintLine(TEXT("Press Enter to continue..."));
 
-    InitGame();
+    SetupGame();
    
 }
 
@@ -16,18 +16,24 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 {
     ClearScreen();
 
+    HiddenWord.Len(); 
+
     if (Input == HiddenWord)
     {
         PrintLine(TEXT("You Win"));
     }
     else
     {
+        if (Input.Len() != HiddenWord.Len())
+        {
+            PrintLine(TEXT("Hidden word is 4 characters long,try again"));
+        }
         PrintLine(TEXT("You Lose!"));
     }
 
 }
 
-void UBullCowCartridge::InitGame()
+void UBullCowCartridge::SetupGame()
 {
     HiddenWord = TEXT("cake");
     Lives = 4 ;
