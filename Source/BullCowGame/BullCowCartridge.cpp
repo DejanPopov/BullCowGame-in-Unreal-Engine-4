0d3063d3ca8c757931df31a823f5ea7f5b41303d
Dejan Popov <dekipopica92@gmail.com>
2020-09-10 19:12:36 +0200

    Added Lives
    Added lives decreasing when player types wrong word until they reach zero,then they lose

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,41 +5,59 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     SetupGame();
+    EndGame();
 
     //Debug line
     PrintLine(TEXT("The HiddenWord is %s.\nIt is %i chars long."), *HiddenWord,
         HiddenWord.Len());
 
-    PrintLine(TEXT("Hello there!"));
-    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len()); //Number is hardcoded REMOVE latter!
-    PrintLine(TEXT("Type in your guess and press Enter to continue..."));
-    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
-
-    HiddenWord.Len(); 
-
-    if (Input == HiddenWord)
+    if (bGameOver)      
     {
-        PrintLine(TEXT("You Win"));
+        ClearScreen();
+        SetupGame();
     }
     else
     {
-        if (Input.Len() != HiddenWord.Len())
+        if (Input == HiddenWord)
         {
-            PrintLine(TEXT("Hidden word is %i characters long,try again")
-            , HiddenWord.Len());
+            PrintLine(TEXT("You Win"));
+            EndGame();
         }
-        PrintLine(TEXT("You Lose!"));
+        else
+        {
+            --Lives;
+            if (Lives > 0)
+            {
+                if (Input.Len() != HiddenWord.Len())
+                {
+                    PrintLine(TEXT("Sorry,try again! You have %i lives remaining"), Lives);
+                }
+            }
+        }
     }
-
 }
 
 void UBullCowCartridge::SetupGame()
 {
+    //Welcoming Player
+    PrintLine(TEXT("Welcome to BullCow game"));
+
     HiddenWord = TEXT("cake");
-    Lives = 4 ;
+    Lives = HiddenWord.Len();
+    bGameOver = false;
+
+    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len()); //Number is hardcoded REMOVE latter!
+    PrintLine(TEXT("Type in your guess. \npress Enter to continue..."));
+    PrintLine(TEXT("You have %i lives left"), Lives);
+
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Press Enter to play again"));
 }
